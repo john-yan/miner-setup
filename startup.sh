9b0630d2e128ed5bdec4be3207fc09577875e09c
miner <miner@Miner.lan>
2018-06-28 21:01:26 -0500

    update oc-config

--- startup.sh
+++ startup.sh
@@ -15,9 +15,9 @@ source /oc/oc-base.sh
 setup_all_gpus /oc/oc-config.txt
 
 # reboot after 24 hrs
-bash -c "sleep 86400 && reboot" &
+# bash -c "sleep 86400 && reboot" &
 
-#export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7,8,9
+#export CUDA_VISIBLE_DEVICES=0,1,2,4,5,6,7,8,9,10,11
 
 # start miner
 MINER_NAME=miner
@@ -26,6 +26,6 @@ MINER_HOME=/home/$MINER_NAME
 #su miner1 -c "screen -dmS mine /home/miner1/zec-miner/start2.sh"
 su $MINER_NAME -c "screen -dmS mine $MINER_HOME/claymore-11.6/start.bash"
 su $MINER_NAME -c "screen -S mine -X screen sudo bash $MINER_HOME/miner-setup/gpu-monitor.sh"
-su $MINER_NAME -c "screen -S mine -X screen sudo $MINER_HOME/OhGodAnETHlargementPill-r2"
+#su $MINER_NAME -c "screen -S mine -X screen sudo $MINER_HOME/OhGodAnETHlargementPill-r2"
 
 exit 0
